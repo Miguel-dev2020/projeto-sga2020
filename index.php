34d7272c58ea0afa258ace7553493d4c36377cf8
migue <migue@DESKTOP-7PNHQQS>
2019-12-26 15:53:02 -0100

    Fazer commit do projeto SGA2020

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>SGA - index</title>
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    </head>
+    <body>
+        <div clas="row justify-content-center">
+        <form action="" method="POST">
+            <div class="form-group">
+        <label>Name:</label>
+        <input type="text" name="name" class="form-control" value="Enter you name">
+            </div>
+            <div class="form-group">
+        <label>Location:</label>
+        <input type="text" name="location" class="form-control" value="Enter you location">
+            </div>
+            <div class="form-group">
+            <button type="submit" calss="btn btn-primary" name="guardar">Guardar</button>
+            <button type="reset" calss="btn btn-primary" name="limpar">Limpar</button>
+            
+            </div>
+        </form>
+        </div>
+    </body>
+</html>

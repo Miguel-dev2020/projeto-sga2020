1087533ff24871525fc3e35b8afbe9e7a9b7f6d7
migue <migue@DESKTOP-7PNHQQS>
2019-12-30 09:58:52 -0100

    Fazer commit do projeto SGA2020

--- index.php
+++ index.php
@@ -7,22 +7,116 @@
     
     </head>
     <body>
-        <div clas="row justify-content-center">
-        <form action="" method="POST">
-            <div class="form-group">
-        <label>Name:</label>
-        <input type="text" name="name" class="form-control" value="Enter you name">
-            </div>
-            <div class="form-group">
-        <label>Location:</label>
-        <input type="text" name="location" class="form-control" value="Enter you location">
-            </div>
-            <div class="form-group">
-            <button type="submit" calss="btn btn-primary" name="guardar">Guardar</button>
-            <button type="reset" calss="btn btn-primary" name="limpar">Limpar</button>
-            
-            </div>
-        </form>
+        <?php require_once 'processa.php'; ?>
+        
+        <?php 
+        //Alerta de menssagem
+        if (isset($_SESSION['message'])): ?>
+        <div class="alert alert-<?=$_SESSION['msg_type']?>">
+            <?php 
+             echo $_SESSION['message'];
+             unset($_SESSION['message']); 
+            ?>
+        </div>
+        <?php endif ?>
+        <div class="container">
+        <?php 
+        $mysqli = new mysqli('localhost', 'root', '', 'bd-sga') or die (mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM utilizadores") or die ($mysqli->error);
+        //pre_r($result);
+        //pre_r($result ->fetch_assoc());
+        ?>
+        <div class="row justify-content-center">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Utilizador</th>
+                        <th>Email</th>
+                        <th>Data Criação</th>
+                        <th>Situação</th>
+                        <th>Nivel de Acesso</th>
+                        <th>Data Modificação</th>
+                        <th colspan="2">Ações</th>
+                    </tr>
+                </thead>
+                <?php 
+                        while ($row = $result->fetch_assoc()):?>
+                <tr>
+                    <td><?php echo $row['utilizador']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['dt_criacao']; ?></td>
+                    <td><?php echo $row['situacoe_id']; ?></td>
+                    <td><?php echo $row['niveis_acesso_id']; ?></td>
+                    <td><?php echo $row['dt_modificacao']; ?></td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['id']; ?>"
+                           class="btn btn-info">Editar</a>
+                        <a href="processa.php?delete=<?php echo $row['id']; ?>"
+                           class="btn btn-danger">Eliminar</a>
+                    </td>
+                        
+                    
+                </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
+        
+                <?php
+                function pre_r($array){
+
+                    echo '<pre>';
+                    print_r($array);
+                    echo '<pre>';
+                }
+                ?>
+
+        <div class="row justify-content-center">
+            <form action="processa.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <div class="form-group">
+            <label>utilizador:</label>
+            <input type="text" name="utilizador" class="form-control" value="<?php echo $utilizador; ?>" placeholder="Utilizador">
+                </div>
+                <div class="form-group">
+            <label>email:</label>
+            <input type="email" name="email" class="form-control" value="<?php echo $email; ?>"placeholder="email">
+                </div>
+                <div class="form-group">
+            <label>Senha:</label>
+            <input type="password" name="senha" class="form-control" value="<?php echo $senha; ?>" placeholder="Senha">
+                </div>                
+                <div class="form-group">
+            <label>Situação:</label>
+            <input type="text" name="situacoe_id" class="form-control" value="<?php echo $situacoe_id; ?>" placeholder="situação">
+                </div>
+                 <div class="form-group">
+            <label>Niveil de Acesso:</label>
+            <input type="text" name="niveis_acesso_id" class="form-control" value="<?php echo $niveis_acesso_id; ?>" placeholder="Nivel de Acesso">
+                </div>
+                 <div class="form-group">
+            <label>Data Criação:</label>
+            <input type="timestamp" name="dt_criacao" class="form-control" value="<?php echo $dt_criacao; ?>" placeholder="Data de Criação">
+                </div>               
+                  <div class="form-group">
+            <label>Data Modificação:</label>
+            <input type="timestamp" name="dt_modificacao" class="form-control" value="<?php echo $dt_modificacao; ?>" placeholder="Data de Modificação">
+                </div>              
+                <div class="form-group">
+                    
+                   <?php 
+                   //alterar o botão para atualizar
+                   if ($update == true):
+                   
+                   ?>
+                <button type="submit" calss="btn btn-info" name="update">Atualizar</button>
+                <?php else: ?>
+                <button type="submit" calss="btn btn-info" name="Guardar">Guardar</button>
+                <button type="reset" calss="btn btn-primary" name="Limpar">Limpar</button>
+                <?php endif;?>
+
+                </div>
+        </div>
+            </form>
         </div>
     </body>
 </html>
---
+++ processa.php
@@ -0,0 +1,84 @@
+<?php
+//start de sessão
+session_start();
+//conexão com a base de dados
+$mysqli = new mysqli('localhost', 'root', '', 'bd-sga') or die (mysqli_error($mysqli));
+    $id=0;
+    $update = false;
+    //atribuir o valor vaziu para os campos do formulário
+    $utilizador = '';
+    $email = '';
+    $senha = '';
+    $situacoe_id = '';
+    $niveis_acesso_id = '';
+    $dt_criacao = '';
+    $dt_modificacao = '';
+    if (isset($_POST['guardar'])){
+        $utilizador = $_POST['utilizador'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $situacoe_id = $_POST['situacoe_id'];
+        $niveis_acesso_id = $_POST['niveis_acesso_id'];
+        $dt_criacao = $_POST['dt_criacao'];
+        $dt_modificacao = $_POST['dt_modificacao'];
+        //mensagem do resgistro guardado
+        $_SESSION['message'] = "Registro foi bem guardado!"; 
+        $_SESSION['msg_type'] = "success!";
+
+        //Voltar para a pagina index
+        header("location: index.php");
+        
+        //enviar dados para a base de dados  
+            $mysqli->query("INSERT INTO utilizadores (utilizador, email, senha, situacoe_id, niveis_acesso_id, dt_criacao, dt_modificacao)VALUES('$utilizador', '$email', '$senha', '$situacoe_id', '$niveis_acesso_id', '$dt_criacao', '$dt_modificacao')") 
+                    or die($mysqli->error);
+    }
+    //apagar dados da base de dados 
+    if (isset($_GET['delete'])){
+        $id = $_GET['delete'];
+        $mysqli->query("DELETE FROM utilizadores WHERE id = $id") or die($mysqli->error());
+        
+        //mensagem do resgistro guardado
+        $_SESSION['message'] = "Registro foi bem eliminado!"; 
+        $_SESSION['msg_type'] = "danger!";
+
+        //Voltar para a pagina index
+        header("location: index.php");
+    } 
+    
+    //veficar o botão fazer comparação e atualizar registro na base de dados 
+    if (isset($_GET['edit'])){
+        $id = $_GET['edit'];
+        $update = true;
+        $result = $mysqli->query("SELECT * FROM utilizadores WHERE id = $id") or die($mysqli->error());
+        if(count($result)==1){
+            $row = $result->fetch_array();
+            $utilizador = $row['utilizador'];
+            $email = $row['email'];
+            $senha = $row['senha'];
+            $situacoe_id = $row['situacoe_id'];
+            $niveis_acesso_id = $row['niveis_acesso_id'];
+            $dt_criacao = $row['dt_criacao'];
+            $dt_modificacao = $row['dt_modificacao'];
+        
+    }
+    }
+    if (isset($_POST['update'])){
+        $id = $_POST['id'];
+        $utilizador = $_POST['utilizador'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $situacoe_id = $_POST['situacoe_id'];
+        $niveis_acesso_id = $_POST['niveis_acesso_id'];
+        $dt_criacao = $_POST['dt_criacao'];
+        $dt_modificacao = $_POST['dt_modificacao'];
+        
+        //atualização dos dados
+        $mysqli->query("UPDATE utilizadores SET utilizador='$utilizador', email='$email', senha='$senha', situacoe_id='$situacoe_id', niveis_acesso_id='$niveis_acesso_id', dt_criacao='$dt_criacao', dt_modificacao='$dt_modificacao' WHERE id=$id") or die($mysqli->error);
+    
+        //mensagem do resgistro guardado
+        $_SESSION['message'] = "Registro foi bem atualizado!"; 
+        $_SESSION['msg_type'] = "warning!";
+        //Voltar para a pagina index
+        header("location: index.php");
+    }
+?>
